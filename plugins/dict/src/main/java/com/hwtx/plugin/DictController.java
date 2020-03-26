32679aa907458a454f850d18adf0a9045451baf7
warning5 <panye02@meituan.com>
2020-03-26 16:09:44 +0800

    调整maven依赖

--- plugins/dict/src/main/java/com/hwtx/plugin/DictController.java
+++ plugins/dict/src/main/java/com/hwtx/plugin/DictController.java
@@ -6,6 +6,7 @@ import cc.mrbird.febs.common.entity.FebsResponse;
 import cc.mrbird.febs.common.entity.QueryRequest;
 import cc.mrbird.febs.common.exception.FebsException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DictController extends BaseController {
 
+    @Autowired
+    private DictService dictService;
 
     @GetMapping("list")
     @ControllerEndpoint(exceptionMessage = "获取字典列表失败")
     public FebsResponse list(QueryRequest request) throws FebsException {
+        dictService.doIt();
         return new FebsResponse().success();
     }
 }
--- plugins/dict/src/main/java/com/hwtx/plugin/DictService.java
+++ plugins/dict/src/main/java/com/hwtx/plugin/DictService.java
@@ -1,4 +1,16 @@
 package com.hwtx.plugin;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author warning5
+ */
+@Service
+@Slf4j
 public class DictService {
+
+    public void doIt() {
+        log.info("do dict service.");
+    }
 }
